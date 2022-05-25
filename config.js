71cb8147634521e129a04ca069380c98d208480d
DARK UMAR <104460597+pindiboy404@users.noreply.github.com>
2022-05-26 00:00:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -33,14 +33,14 @@ global.APIKeys = { // The APIKey is here
 
 // Sticker WM
 
-global.packname = '💋 ᴍɪʟʟɪᴇ 💋'
+global.packname = '💋 UMAR 💋'
 
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://github.com/neera-j/Bot-Files/blob/main/images/millie.jpg'
 
-global.watermark = '© ᴍɪʟʟɪᴇ'
+global.watermark = '© ᴜᴍᴀʀ ᴅᴇᴠIʟ'
 
 global.multiplier = 69 // The higher it is, the harder it is to level up
 
